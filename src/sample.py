f6236cff06080c11f143b6c33247ec1930d9b387
Shota Narisaka <shootacean@gmail.com>
2024-02-21 22:35:46 +0000

    format by ruff

--- src/sample.py
+++ src/sample.py
@@ -1,29 +1,34 @@
 # Anki Connect
 # https://ankiweb.net/shared/info/2055492159
 # ANKI_CONNECT_URL = 'http://localhost:8765'
-ANKI_CONNECT_URL = 'http://host.docker.internal:8765'
+ANKI_CONNECT_URL = "http://host.docker.internal:8765"
 
 # below code is sample from https://foosoft.net/projects/anki-connect/
 
 import json
 import urllib.request
 
+
 def request(action, **params):
-    return {'action': action, 'params': params, 'version': 6}
+    return {"action": action, "params": params, "version": 6}
+
 
 def invoke(action, **params):
-    requestJson = json.dumps(request(action, **params)).encode('utf-8')
-    response = json.load(urllib.request.urlopen(urllib.request.Request(ANKI_CONNECT_URL, requestJson)))
+    requestJson = json.dumps(request(action, **params)).encode("utf-8")
+    response = json.load(
+        urllib.request.urlopen(urllib.request.Request(ANKI_CONNECT_URL, requestJson))
+    )
     if len(response) != 2:
-        raise Exception('response has an unexpected number of fields')
-    if 'error' not in response:
-        raise Exception('response is missing required error field')
-    if 'result' not in response:
-        raise Exception('response is missing required result field')
-    if response['error'] is not None:
-        raise Exception(response['error'])
-    return response['result']
+        raise Exception("response has an unexpected number of fields")
+    if "error" not in response:
+        raise Exception("response is missing required error field")
+    if "result" not in response:
+        raise Exception("response is missing required result field")
+    if response["error"] is not None:
+        raise Exception(response["error"])
+    return response["result"]
+
 
 # invoke('createDeck', deck='test1')
-result = invoke('deckNames')
-print('got list of decks: {}'.format(result))
+result = invoke("deckNames")
+print("got list of decks: {}".format(result))
