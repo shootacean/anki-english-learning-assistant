4a0f2286ab03b34eacd87cdbfc11cb1a8713649b
Shota Narisaka <shootacean@gmail.com>
2024-02-23 04:44:09 +0000

    refactor

--- src/anki_english_learning_assistant/__init__.py
+++ src/anki_english_learning_assistant/__init__.py
@@ -7,29 +7,29 @@ import urllib.request
 ANKI_CONNECT_URL = "http://host.docker.internal:8765"
 
 
-def request(action, **params):
-    return {"action": action, "params": params, "version": 6}
+class Anki:
+    def __request(self, action, **params):
+        return {"action": action, "params": params, "version": 6}
 
+    def __invoke(self, action, **params):
+        requestJson = json.dumps(self.__request(action, **params)).encode("utf-8")
+        response = json.load(
+            urllib.request.urlopen(
+                urllib.request.Request(ANKI_CONNECT_URL, requestJson)
+            )
+        )
+        if len(response) != 2:
+            raise Exception("response has an unexpected number of fields")
+        if "error" not in response:
+            raise Exception("response is missing required error field")
+        if "result" not in response:
+            raise Exception("response is missing required result field")
+        if response["error"] is not None:
+            raise Exception(response["error"])
+        return response["result"]
 
-def invoke(action, **params):
-    requestJson = json.dumps(request(action, **params)).encode("utf-8")
-    response = json.load(
-        urllib.request.urlopen(urllib.request.Request(ANKI_CONNECT_URL, requestJson))
-    )
-    if len(response) != 2:
-        raise Exception("response has an unexpected number of fields")
-    if "error" not in response:
-        raise Exception("response is missing required error field")
-    if "result" not in response:
-        raise Exception("response is missing required result field")
-    if response["error"] is not None:
-        raise Exception(response["error"])
-    return response["result"]
+    def create_deck(self, **params):
+        return self.__invoke("createDeck", **params)
 
-
-def create_deck(**params):
-    return invoke("createDeck", **params)
-
-
-def deck_names(**params):
-    return invoke("deckNames", **params)
+    def deck_names(self, **params):
+        return self.__invoke("deckNames", **params)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-import anki_english_learning_assistant as anki
+from anki_english_learning_assistant import Anki
 
 # TODO : Anki Connectが起動しているかのチェック
+
+anki = Anki()
 
 # Create deck
 anki.create_deck(deck="anki-english-learning-assistant")
@@ -8,3 +10,5 @@ anki.create_deck(deck="anki-english-learning-assistant")
 # List decks
 result = anki.deck_names()
 print("got list of decks: {}".format(result))
+
+# TODO : add note
